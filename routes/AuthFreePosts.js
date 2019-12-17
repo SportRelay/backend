13cc9b0ca168c932fcbfe9834e76b68940e5685c
MSalah73 <ms24@pdx.edu>
2019-12-17 23:40:39 +0300

    Delete post added

--- routes/AuthFreePosts.js
+++
@@ -1,1 +0,0 @@
-AuthFreePosts.js
--- routes/Post.js
+++ routes/Post.js
@@ -9,7 +9,8 @@ const passportHelper = require('../helper/passport')
 
 router.get("/", async (req, res) => {
   try{
-    const posts = await Post.find();
+    let posts = await Post.find().populate("user_id");
+    posts = posts.map( item => {item.user_id.password = item.user_id.firstname = item.user_id.lastname = item.user_id.email = "";  return item});
     res.status(200).json({posts: posts});
    }catch(err){
      res.status(401).json({ message : "Site has no posts..."})
@@ -62,6 +63,31 @@ router.put("/comment/:postId", (req, res)  =>{
 				res.status(404).json({message: "post not found"});
 			}
 			res.status(201).json({message: user.username+"'s comment added"});
+		}catch(err){
+			res.status(401).json({ message : err})
+
+		}
+	}
+  })(req, res)
+})
+router.delete("/:postId", (req, res) => {
+	passport.authenticate('jwt', {session: false}, async (err, user, info)=>{
+	  if(err){ return res.status(400).json({ message: err }) }
+
+      if(info !== undefined){
+        return res.json({ message: info.message })
+      }else{
+		try{
+			const userId = getUserId(req);
+			let user = await User.findById(userId);
+			user.posts.splice((user.posts.indexOf(req.params.postId)), 1);
+			user = await User.update({ _id: userId },{ $set: {posts: user.posts}});
+			const post = await Post.findByIdAndDelete(req.params.postId);
+			if(!post){
+				res.status(404).json({message: "Post not found"});
+			}else{
+				res.json({message: "Post Deleted", post});
+			}
 		}catch(err){
 			res.status(401).json({ message : err})
 
